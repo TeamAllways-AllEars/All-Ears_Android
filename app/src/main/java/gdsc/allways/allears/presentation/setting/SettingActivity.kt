d9300b8e5100694bdd824b58e31c07d32cfd466c
yeji0214 <1225yj50@naver.com>
2024-02-22 17:21:00 +0900

    font 변경

--- app/src/main/java/gdsc/allways/allears/presentation/setting/SettingActivity.kt
+++ app/src/main/java/gdsc/allways/allears/presentation/setting/SettingActivity.kt
@@ -1,8 +1,13 @@
 package gdsc.allways.allears.presentation.setting
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import gdsc.allways.allears.R
 import gdsc.allways.allears.databinding.ActivitySettingBinding
+import gdsc.allways.allears.presentation.decibel.VoiceRecognitionActivity
 
 class SettingActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySettingBinding
@@ -16,6 +21,34 @@ class SettingActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         // "SettingActivity" 텍스트 설정
-        binding.textView.text = "SettingActivity"
+        //binding.textView.text = "SettingActivity"
+
+        showStartProcessingPopup()
+    }
+
+    private fun showStartProcessingPopup() {
+        val dialogView = layoutInflater.inflate(R.layout.popup_layout, null)
+
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setView(dialogView)
+
+        val alertDialog = alertDialogBuilder.create()
+
+        val yesButton = dialogView.findViewById<Button>(R.id.yes_button)
+        val noButton = dialogView.findViewById<Button>(R.id.no_button)
+
+        yesButton.setOnClickListener {
+            // 사용자가 Yes를 선택한 경우 VoiceRecognitionActivity로 이동
+            val intent = Intent(this@SettingActivity, VoiceRecognitionActivity::class.java)
+            startActivity(intent)
+            alertDialog.dismiss()
+        }
+
+        noButton.setOnClickListener {
+            // 사용자가 No를 선택한 경우의 처리 로직을 여기에 추가
+            alertDialog.dismiss()
+        }
+
+        alertDialog.show()
     }
 }
